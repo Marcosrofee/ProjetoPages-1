9369159ecaa9b342b92fd0146d25c9d4d710e398
Marcosrofee <marcroger61@gmail.com>
2023-07-31 10:46:17 -0300

    up
    modificacao

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+var setaDireita= window.document.getElementById("seta-dereita")
+var leonardo = window.document.getElementById("leo")
+var samantha = window.document.getElementById("sam")
+var bruna = window.document.getElementById("bru")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+
+    function RolarParaDireita() {
+    leo.style = "display:none"
+    bru.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex" }
+ 
+    function RolarEsquerda(){
+    leo.style="display:flex;"  
+    bru.style="display:none"  
+    setaDireita.style="display:flex"
+    setaEsquerda.style="display:none;"}
